428266d911302589d5169d6b1882eb5ceccb54ca
PabloSanhueza1 <Sanhueza.pem@gmail.com>
2022-10-24 15:35:20 -0300

    Revert "metodo CalcularVuelto"
    This reverts commit ea95a23c7d3a24e92c82c50c2f1266e8d4b433b7.

--- Tarea2/src/tarea2/Expendedor.java
+++ Tarea2/src/tarea2/Expendedor.java
@@ -3,7 +3,6 @@ package tarea2;
 // abstract de la clase `Expendedor`
 // Metodos
 // - 
-
 class Expendedor {
 
     private DepositoBebida cocacola;
@@ -21,16 +20,23 @@ class Expendedor {
         for (int i = 0; i < numBebidas; i++) {
             Bebida cocacola = new CocaCola(100 + i);
             this.cocacola.addBebida(cocacola);
-            Bebida spritee = new Sprite(200 + i);
-            this.sprite.addBebida(spritee);
+            Bebida sprite = new Sprite(200 + i);
+            this.sprite.addBebida(sprite);
             Bebida fanta = new Fanta(300 + i);
             this.fanta.addBebida(fanta);
         }
     }
 
+    public void calcVuelto(Moneda moneda) {
+        int vuelto = moneda.getValor() - precioBebidas;
+        for (int i = 0; i < vuelto / 100; i++) {
+            vueltoTotal.add();
+        }
+    }
+
     public Bebida comprarBebida(Moneda moneda, int cual) {
         Bebida bebida = null;
-        if(moneda == null) {
+        if (moneda == null) {
             //PagoIncorrectoException
         } else {
             if (moneda.getValor() >= precioBebidas) {
@@ -38,45 +44,35 @@ class Expendedor {
                 switch (cual) { //vuelto es la moneda a devolver falta definirla
                     case 1:
                         bebida = cocacola.getBebida();
-                        calcularVuelto(moneda);
+                        //metodo calcular `vuelto` entregando como parametro el valor de la bebida y la moneda.getValor en monedas de 100 en el deposito
+                        calcVuelto(moneda);
                         return bebida;
                     case 2:
                         bebida = sprite.getBebida();
-                        calcularVuelto(moneda);
+                        calcVuelto(moneda);
                         return bebida;
                     case 3:
                         bebida = fanta.getBebida();
-                        calcularVuelto(moneda);
+                        calcVuelto(moneda);
                         return bebida;
                     default:
-                        vueltoTotal.add(moneda);
                         //imprimir excepcion
                         return null;
                 }
+
             } else if (moneda.getValor() < precioBebidas) {
                 //PagoInsuficienteException
             }
         }
+
         return null;
     }
 
-    public void calcularVuelto(Moneda moneda) {
-        int monedas100 = (moneda.getValor() - precioBebidas) / 100;
-        for (int i = 0; i < monedas100; i++) {
-            Moneda monedaVuelto = new Moneda100();
-            vueltoTotal.add(monedaVuelto);
-        }
-    }
-    
-    public Moneda getVuelto(){ //torna moneda, null si deposito está vacío
+    public Moneda getVuelto() { //torna moneda, null si deposito está vacío
         return vueltoTotal.getVuelto(); // se rescatan una a una
     }
 
-    public int getPrecioBebidas(){
+    public int getPrecioBebidas() {
         return precioBebidas;
     }
 }
-/* 
- * Features:
- * Metodo calcularVuelto() entregando como parametro la moneda.getValor convierte y deposita en monedas de 100 en el deposito
- */
