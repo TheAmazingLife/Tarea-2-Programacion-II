8fbf37c42a10607bf51e6f70de200ad87965153f
Joaquin <joaquin.san.vargas19@gmail.com>
2022-10-25 16:45:15 -0300

    Creacion e implementacion de Excepciones

--- Tarea2/src/tarea2/Tarea2.java
+++ Tarea2/src/tarea2/Tarea2.java
@@ -17,15 +17,5 @@ package tarea2;
 
 public class Tarea2 {
 
-    public static void main(String[] args) {
-        Moneda mon1 = new Moneda1500();
-        Expendedor exp = new Expendedor(10, 1200);
-        /* bebida tipo 1: CocaCola
-        bebida tipo 2: Sprite
-        bebida tipo 3: Fanta */
-        Comprador pabloComprador = new Comprador(mon1, 1, exp);
-        System.out.println(pabloComprador.cuantoVuelto());
-        System.out.println(pabloComprador.queBebiste());
-    }
 
 }
