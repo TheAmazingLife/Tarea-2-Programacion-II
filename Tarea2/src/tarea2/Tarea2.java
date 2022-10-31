7adcaa8a91ba6b5013d6ab5dfb73a2105aa76953
PabloSanhueza1 <Sanhueza.pem@gmail.com>
2022-10-30 22:43:10 -0300

    Main terminado

--- Tarea2/src/tarea2/Tarea2.java
+++ Tarea2/src/tarea2/Tarea2.java
@@ -24,31 +24,51 @@ public class Tarea2 {
      */
     public static void main(String[] args) {
 
-        Moneda mon1 = null;
-        Expendedor exp1 = new Expendedor(10, 1200);
-        Comprador pabloComprador = new Comprador(mon1, 1, exp1);
-        System.out.println(pabloComprador.cuantoVuelto());
-        System.out.println(pabloComprador.queBebiste() + "\n");
+        /* 
+        *   expendedor con:
+        *   - una bebida de cada tipo
+        *   - cada bebida se vende por $900 CLP
+        */
+        Expendedor exp1 = new Expendedor(1, 900);
 
-        Moneda mon2 = new Moneda100();
+        // compra con moneda de $1500 y realizada con exito, sabor bebida CocaCola (1) y Vuelto $600
+        Moneda mon1 = new Moneda1500();
+        System.out.println(mon1.toString());        
+        Comprador joaquinComprador = new Comprador(mon1, 1, exp1);
+        System.out.println("Vuelto: $" + joaquinComprador.cuantoVuelto());
+        System.out.println("Sabor: " + joaquinComprador.queBebiste() + "\n");
+
+        // compra con moneda de $1000 y realizada con exito, sabor bebida Sprite (2) y Vuelto $100
+        Moneda mon2 = new Moneda1000();
         System.out.println(mon2.toString());
-        Expendedor exp2 = new Expendedor(10, 1200);
-        Comprador joaquinComprador = new Comprador(mon2, 1, exp2);
-        System.out.println(joaquinComprador.cuantoVuelto());
-        System.out.println(joaquinComprador.queBebiste() + "\n");
+        Comprador pabloComprador = new Comprador(mon2, 2, exp1);
+        System.out.println("Vuelto: $" + pabloComprador.cuantoVuelto());
+        System.out.println("Sabor: " + pabloComprador.queBebiste() + "\n");
 
+        // compra con moneda de $1500 y no realizada con exito, sabor bebida CocaCola (1) no disponible y Vuelto $1500 su moneda
+        // retorno excepcion NoHayBebidaException
         Moneda mon3 = new Moneda1500();
-        Expendedor exp3 = new Expendedor(0, 1200);
-        Comprador geoffreyComprador = new Comprador(mon3, 1, exp3);
-        System.out.println(geoffreyComprador.cuantoVuelto());
-        System.out.println(geoffreyComprador.queBebiste() + "\n");
+        System.out.println(mon3.toString());
+        Comprador lissetteComprador = new Comprador(mon3, 1, exp1);
+        System.out.println("Vuelto: $" + lissetteComprador.cuantoVuelto());
+        System.out.println("Sabor: " + lissetteComprador.queBebiste() + "\n");
 
-        Moneda mon4 = new Moneda1500();
+        // compra con moneda de menor precio $500 y compra no realizada con exito, no puede comprar con moneda menor y Vuelto su moneda $500
+        // retorno excepcion PagoIncorrectoException
+
+        Moneda mon4 = new Moneda500();
         System.out.println(mon4.toString());
-        Expendedor exp4 = new Expendedor(10, 1200);
-        Comprador lissetteComprador = new Comprador(mon4, 1, exp4);
-        System.out.println(lissetteComprador.cuantoVuelto());
-        System.out.println(lissetteComprador.queBebiste() + "\n");
+        Comprador EstebanComprador = new Comprador(mon4, 3, exp1);
+        System.out.println("Vuelto: $" + EstebanComprador.cuantoVuelto());
+        System.out.println("Sabor: " + EstebanComprador.queBebiste() + "\n");
+
+        // compra sin moneda y no realizada con exito, no puede comprar con moneda nula y Vuelto su moneda nula
+        // retorno excepcion PagoInsuficienteException
+
+        Moneda mon5 = null;
+        Comprador lukasComprador = new Comprador(mon5, 3, exp1);
+        System.out.println("Vuelto: $" + lukasComprador.cuantoVuelto());
+        System.out.println("Sabor: " + lukasComprador.queBebiste() + "\n");
     }
 
 }
